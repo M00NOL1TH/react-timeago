8da0ceceecc9398e5f44567aeb4402ffc5046c90
András Hegyi <andras@milinnovations.com>
2017-10-30 14:22:31 +0100

    Add TS typing

---
+++ src/index.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="react" />
+
+declare module "react-timeago" {
+    export type Unit = "second" | "minute" | "hour" | "day" | "week" | "month" | "year";
+    export type Suffix = "ago" | "from now";
+
+    export type Formatter =
+        (value: number, unit: Unit, suffix: Suffix, epochSeconds: number, nextFormatter?: Formatter) => string | React.ReactElement<any>;
+
+    export interface TimeAgoProps extends React.HTMLAttributes<TimeAgo> {
+        date: Date | string | number;
+        live?: boolean;
+        formatter?: Formatter;
+        component?: React.ReactType;
+        title?: string;
+        minPeriod?: number;
+        maxPeriod?: number;
+    }
+
+    export default class TimeAgo extends React.Component<TimeAgoProps, {}> { }
+}
